50d4636ac58a6185fceb3557a0b13ce96c98a90c
iMtRo <maxmudovilyos75@gmail.com>
2022-01-12 03:55:02 +0500

    Initial Commit

---
+++ ShopUI/ShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIApp.swift
+//  ShopUI
+//
+//  Created by user on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
